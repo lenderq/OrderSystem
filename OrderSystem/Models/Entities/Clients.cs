2e1dd50fd10cd76c3da64c1b7e109eb862780706
Леонид Конашенков <konashenkov.lenya@gmail.com>
2025-03-31 16:27:05 +0300

    Initial commit

---
+++ OrderSystem/Models/Entities/Clients.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderSystem.Models.Entities
+{
+    public class Clients
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public List<Order> Orders { get; set; } = new();
+    }
+}
